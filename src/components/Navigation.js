af9ce35fdcc428e80dda46c697584ee2aadd8593
Klefebvre <klefebvre.dev@gmail.com>
2021-05-06 11:34:47 +0200

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <nav className="navigation">
+        <ul>
+          <li>Accueil</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Connexion</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
